b9cac1f975179f457976621c576cc323d54e34d4
Adelson <adelsoncouto2@gmail.com>
2026-07-13 13:07:04 -0300

    Adição do carrosel da pagina inicial

--- js/header.js
+++ js/header.js
@@ -1,17 +1,21 @@
 fetch("../components/header.html")
-    .then(r => r.text())
-    .then(html => {
-        document.getElementById("header").innerHTML = html;
-        const pagina = window.location.pathname.split("/").pop() || "index.html";
-        document.querySelectorAll(".header-text a.text").forEach(link => {
-            const href = link.getAttribute("href").split("/").pop()
-            if (href === pagina) {
-                link.classList.remove("text");
-                link.classList.add("text-selected");
-            }
-        });
-        const vai = document.querySelector(".vai-container");
-        if (pagina === "vai.html") {
-            vai.classList.add("active");
-        }
+  .then((r) => {
+    if (!r.ok) throw new Error("Erro ao carregar");
+    return r.text();
+  })
+  .then((html) => {
+    document.getElementById("header").innerHTML = html;
+    const pagina = window.location.pathname.split("/").pop() || "index.html";
+    document.querySelectorAll(".header-text a.text").forEach((link) => {
+      const href = link.getAttribute("href").split("/").pop();
+      if (href === pagina) {
+        link.classList.remove("text");
+        link.classList.add("text-selected");
+      }
     });
+    const vai = document.querySelector(".vai-container");
+    if (pagina === "vai.html" && vai) {
+      vai.classList.add("active");
+    }
+  })
+  .catch((err) => console.error(err));
